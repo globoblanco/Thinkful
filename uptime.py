318874b56fb34c76988dcd2aefaec99b36607ca1
globoblanco <41493246+globoblanco@users.noreply.github.com>
2018-11-14 17:27:10 -0700

    Add files via upload
    Added a Python script to display server uptime.

---
+++ uptime.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 20 22:41:47 2018
+
+@author: Owner
+"""
+
+import subprocess
+
+def uptime():
+    raw = subprocess.check_output('uptime').replace(',','')
+    days = int(raw.split()[2])
+    if 'min' in raw:
+    	hours = 0
+    	minutes = int(raw[4])
+    else:
+    	hours, minutes = map(int,raw.split()[4].split(':'))
+    totalsecs = days*24*60*60 + hours*60*60 + minutes*60    
+    return totalsecs
+    
+print('System uptime of %d seconds' % (uptime()))
